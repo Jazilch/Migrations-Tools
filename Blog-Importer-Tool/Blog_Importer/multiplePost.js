32097275f5b976036d10a90a79bc54ee5eae141e
Jazilch <zilchjames5@gmail.com>
2018-02-01 17:22:42 -0500

    Update multiplePost.js

--- Blog-Importer-Tool/Blog_Importer/multiplePost.js
+++ Blog-Importer-Tool/Blog_Importer/multiplePost.js
@@ -1,50 +1,78 @@
 'use strict';
 require('dotenv').config({ path: 'variables.env' });
 const axios = require('axios');
+const cliUtils = require('./cliutils');
 
-const singlePost = 'https://api.hubapi.com/content/api/v2/blog-posts/5411148913?access_token=' + process.env.ACCESS_TOKEN;
-const allPosts =   'https://api.hubapi.com/content/api/v2/blog-posts?access_token=' + process.env.ACCESS_TOKEN;
-const putURL = 'https://api.hubapi.com/content/api/v2/blog-posts/5411148913?access_token=' + process.env.ACCESS_TOKEN;
+cliUtils.showFiglet();
+cliUtils.getUserPreferences(function(answers) {
+    let parentBlogID = answers.parentBlogID;
+    let fieldName = answers.fieldName;
+    console.log(parentBlogID);
+    console.log(fieldName);
 
-var fieldName = "et_pb";
-var regExp = new RegExp("\\[\/?(" + fieldName + ".*?)\\]", "g");
+    let endpointSingleBlogPost = `https://api.hubapi.com/content/api/v2/blog-posts/:blog_post_id?access_token=` + process.env.ACCESS_TOKEN;
+    console.log(endpointSingleBlogPost);
+    let endpointAllBlogPosts =   `https://api.hubapi.com/content/api/v2/blog-posts?content_group_id=${parentBlogID}&access_token=` + process.env.ACCESS_TOKEN;
+    console.log(endpointAllBlogPosts);
 
+    let regExp = new RegExp("\\[\/?(" + fieldName + ".*?)\\]", "g");
 
+    axios.get(endpointAllBlogPosts)
+        .then(function(response) {
 
-  axios.get(allPosts {
-        transformResponse: axios.defaults.transformResponse.concat(function(data, headers) {
-            // use data I passed into the function and the objects from the API
-            // pass in data into the function using forEach this will return an array
-            data.objects.forEach(function(i) {
-                // use the returned array on Objects.key to find the name of the array
-                Object.keys(i).forEach(function(k) {
-                    // if the key equals execute code
-                    // console.log(k);
-                    if (k === "post_body") {
-                        // fire Regex
-                        data[k] = i[k].replace(regExp, '');
-                        console.log(data[k])
+            let blogPostIndex = (-1);
+            let contents = response.data.objects;
+
+            function blogPostLoop() {
+                setTimeout(function () {
+                    blogPostIndex++;
+                    console.log('blogPostIndex: ' + blogPostIndex + ' of ' + contents.length);
+
+                    if(blogPostIndex < contents.length) {
+                    // if(blogPostIndex < 3) {
+
+                        if( (contents[blogPostIndex].parent_blog) && (contents[blogPostIndex].parent_blog.id == parentBlogID)) {
+
+                            // console.log('\n');
+                            // console.log("yep, this is the right blog.");
+
+                            // let's do stuff
+
+                            let postID = contents[blogPostIndex].id;
+                            let postBody = contents[blogPostIndex].post_body;
+                            let postBodyFixed = contents[blogPostIndex].post_body.replace(regExp, '');
+                            let endpointSingleBlogPostThis = endpointSingleBlogPost.replace(':blog_post_id', postID);
+
+                            console.log('postID: ', postID);
+                            // console.log('postBody: ', postBody);
+                            // console.log('postBodyFixed: ', postBodyFixed);
+                            // console.log('endpointSingleBlogPostThis: ', endpointSingleBlogPostThis);
+
+                            if(postBody != postBodyFixed) {
+
+                                // console.log('post body to PUT: ', postBodyFixed);
+                                // console.log(typeof postBodyFixed);
+
+                                axios.put(endpointSingleBlogPostThis, {
+                                    "post_body": postBodyFixed
+                                }).then(function(response) {
+                                    // console.log(response);
+                                    console.log('***************************************');
+                                    console.log("response.status: ", response.status);
+                                    console.log("response.statusText: ", response.statusText);
+                                    console.log('***************************************');
+                                }).catch(function(error) {
+                                    console.log(error);
+                                });
+
+                            }
+                            console.log('\n');
+                        }
+
+                        blogPostLoop();
                     }
-                })
-            })
-            return data;
-        })
-    })
-    .then(function(res) {
-        // console.log(res.data.post_body);
-        res.data.forEach(function(post) {
-            console.log(post)
+                }, 500);
+            }
+            blogPostLoop();
         });
-        // axios({
-        //     method: 'put',
-        //     url: putURL,
-        //     header: {
-        //         "Content-Type": "application/json"
-        //     },
-        //     data: { "post_body":res.data.post_body }
-        // })
-        // if ( res.status == 200 || res.status == 201) {
-        //     console.log("Post Body successfully updated!");
-        // }
-    })
-
+    });
